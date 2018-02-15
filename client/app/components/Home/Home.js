100bafe1aba36f5a01e2819ee46fb323770426e5
Lisandro <lisandro@Lisandros-MacBook-Pro.local>
2018-02-15 11:59:04 -0300

    First client session

---
+++ client/app/components/Home/Home.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import 'whatwg-fetch';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: []
+    };
+
+    this.newCounter = this.newCounter.bind(this);
+    this.incrementCounter = this.incrementCounter.bind(this);
+    this.decrementCounter = this.decrementCounter.bind(this);
+    this.deleteCounter = this.deleteCounter.bind(this);
+
+    this._modifyCounter = this._modifyCounter.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('/api/todos/1')
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+        this.setState({
+          todos: json
+        });
+      });
+  }
+
+  newCounter() {
+    fetch('/api/counters', { method: 'POST' })
+      .then(res => res.json())
+      .then(json => {
+        let data = this.state.counters;
+        data.push(json);
+
+        this.setState({
+          counters: data
+        });
+      });
+  }
+
+  incrementCounter(index) {
+    const id = this.state.counters[index]._id;
+
+    fetch(`/api/counters/${id}/increment`, { method: 'PUT' })
+      .then(res => res.json())
+      .then(json => {
+        this._modifyCounter(index, json);
+      });
+  }
+
+  decrementCounter(index) {
+    const id = this.state.counters[index]._id;
+
+    fetch(`/api/counters/${id}/decrement`, { method: 'PUT' })
+      .then(res => res.json())
+      .then(json => {
+        this._modifyCounter(index, json);
+      });
+  }
+
+  deleteCounter(index) {
+    const id = this.state.counters[index]._id;
+
+    fetch(`/api/counters/${id}`, { method: 'DELETE' })
+      .then(_ => {
+        this._modifyCounter(index, null);
+      });
+  }
+
+  _modifyCounter(index, data) {
+    let prevData = this.state.counters;
+
+    if (data) {
+      prevData[index] = data;
+    } else {
+      prevData.splice(index, 1);
+    }
+
+    this.setState({
+      counters: prevData
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Todos:</p>
+        <ul>
+          {this.state.todos.map( a => (<li key={a._id}>{a.title}</li>) )}
+        </ul>
+        <button onClick={this.newCounter}>New counter</button>
+      </div>
+    );
+  }
+}
+
+export default Home;
